c06e78ae8ed52094e371dd68279e775cb2324f48
Joon <JustcallmeHarmony>
2024-02-21 15:30:26 +0900

    최적화 작업

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
 
@@ -16,9 +16,9 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [date, setDate] = useState(getStringDate(new Date()));
 
   const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
-  const hadleClickEmote = (emotion) => {
+  const hadleClickEmote = useCallback((emotion) => {
     setEmotion(emotion);
-  };
+  }, []);
 
   const navigate = useNavigate();
 
